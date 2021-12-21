8af7f14123da83bfe5c7e1e6a0dda2212af9a447
FABGEMELCK <85943616+FABGEMELCK@users.noreply.github.com>
2021-12-21 17:13:58 -0300

    Add files via upload

---
+++ databese.sql
@@ -0,0 +1,22 @@
+ create table productos 
+ (
+  nombre varchar,
+  precio int,
+  moneda varchar,
+  stock int
+ ),
+select * from productos 
+ insert into productos (nombre,precio,moneda,stock) values 
+ ('Pan Focaccia',180,'UYU',100);
+insert into productos (nombre,precio,moneda,stock) values 
+ ('Pan Barra Rustica',140,'UYU',100);
+insert into productos (nombre,precio,moneda,stock) values 
+ ('Pan Baguette',180,'UYU',100);
+insert into productos (nombre,precio,moneda,stock) values 
+ ('Pan De Camoo',180,'UYU',100);
+
+ select * from productos;
+
+
+
+
